660af02fed9f70b27b26ad042c698416d9978882
Muhammad Kashif Irshad <kashif.irshad@vertex-automation.com>
2019-02-03 23:05:01 +0500

    initial commit

---
+++ vertexsoft/start.bat
@@ -0,0 +1,13 @@
+\env\Scripts\activate.bat
+rem python manage.py migrate
+python manage.py runserver %COMPUTERNAME%:80
+rem start "" http://192.168.8.10:8000/mdc/start_tcp_service
+
+pause
+rem pip install django --upgrade
+rem pip install psycopg2 --upgrade
+rem pip install djangorestframework
+rem python -m pip install --upgrade pip
+rem pip install django-cors-headers
+rem pip install django-rest-auth
+rem pip install django-allauth
